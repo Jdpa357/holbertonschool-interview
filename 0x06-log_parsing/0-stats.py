ba46cfbb2fa8ab6170496d5c97bc4f6c0e45ac4f
jdpa357 <jdpa352@hotmail.es>
2021-04-07 19:02:18 -0500

    Adding additional file to project

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,57 +1,45 @@
 #!/usr/bin/python3
-"""
-Write a script that reads stdin line by line and computes metrics:
-"""
-import sys
+"""Program that reads stdin line by line and computes metrics"""
+from sys import stdin
 
 
-counters = {
-    "size": 0,
-    "lines": 1
+statusCode = {
+    "200": 0,
+    "301": 0,
+    "400": 0,
+    "401": 0,
+    "403": 0,
+    "404": 0,
+    "405": 0,
+    "500": 0
 }
-
-contCode = {
-    "200": 0, "301": 0, "400": 0, "401": 0,
-    "403": 0, "404": 0, "405": 0, "500": 0
-}
+totalSize = 0
 
 
-def printCodes():
-    """
-    function print the codes and the number
-    """
-    # print file size
-    print("File size: {}".format(counters["size"]))
-    # print all codes
-    for key in sorted(contCode.keys()):
-        # if a val is not 0
-        if contCode[key] != 0:
-            print("{}: {}".format(key, contCode[key]))
-
-
-def countCodeSize(listData):
-    """
-    count the codes and file size
-    """
-    # count file size
-    counters["size"] += int(listData[-1])
-    # if exists
-    if listData[-2] in contCode:
-        # count status code
-        contCode[listData[-2]] += 1
+def printStat():
+    """Function that print log accumulately"""
+    print("File size: {}".format(totalSize))
+    for x in sorted(statusCode.keys()):
+        if statusCode[x]:
+            print("{}: {}".format(x, statusCode[x]))
 
 
 if __name__ == "__main__":
+    ct = 0
     try:
-        for line in sys.stdin:
+        for y in stdin:
             try:
-                countCodeSize(line.split(" "))
+                item = y.split()
+                totalSize += int(item[-1])
+                if item[-2] in statusCode:
+                    statusCode[item[-2]] += 1
             except:
                 pass
-            if counters["lines"] % 10 == 0:
-                printCodes()
-            counters["lines"] += 1
+            if ct == 9:
+                printStat()
+                ct = -1
+            ct += 1
     except KeyboardInterrupt:
-        printCodes()
+        printStat()
         raise
-    printCodes()
+    printStat()
